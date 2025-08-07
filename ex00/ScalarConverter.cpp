91946ef5b70527a75e6ef55cd4c56381f2a370f2
aroualid <aroualid@student.42.fr>
2025-08-07 13:16:37 +0200

    first_commit

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,15 @@
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::~ScalarConverter()
+{}
+
+ScalarConverter::ScalarConverter(ScalarConverter const & src)
+{}
+
+ScalarConverter& ScalarConverter::operator=(ScalarConverter const & src)
+{}
+
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+class ScalarConverter {
+
+public:
+	ScalarConverter();
+	ScalarConverter(ScalarConverter const & src);
+	~ScalarConverter();
+	ScalarConverter &operator=(ScalarConverter const & src);
+
+private:
+};
+
+#endif
