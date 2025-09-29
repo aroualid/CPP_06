f92c525788f498ba9ac9ed6ff2bf141fdfe4f3a4
aroualid <aroualid@student.42.fr>
2025-09-29 14:05:19 +0200

    finish

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,13 @@ int main (void)
 	ptr.data = "Salut";
 
 	std::cout << "Ptr adress = " << &ptr << std::endl;
+	std::cout << "Ptr data = " << ptr.data << std::endl;
 
 	uintptr_t adress = Serializer::serialize(&ptr);
+	std::cout << "------------------------------------------------\n";
 	std::cout << "value of uintptr is : " << adress << std::endl;
+	std::cout << "------------------------------------------------\n";
 	Data *newptr = Serializer::deserialize(adress);
-	std::cout << "value of newptr is : " << newptr;
-	std::cout << "\n------------------------------------------------\n";
-	std::cout << "                " << newptr->data << "              ";
-	std::cout << "\n------------------------------------------------\n";
-
+	std::cout << "value of newptr is : " << newptr << std::endl;
+	std::cout << "data of newptr is : " << newptr->data << std::endl;
 }
-
